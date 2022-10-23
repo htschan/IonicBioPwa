6f232c915d0eee0662e92cdee02c6a932fc01b4c
Hans Tschan <hans.tschan@gmail.com>
2022-10-23 16:23:44 +0200

    fix build

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyByfGMja2lco7rSBj1VPJlVmE4UdXSOVAA",
-    authDomain: "myhome-c5350.firebaseapp.com",
-    databaseURL: "https://myhome-c5350.firebaseio.com",
-    projectId: "myhome-c5350",
-    storageBucket: "myhome-c5350.appspot.com",
-    messagingSenderId: "844342741846",
-    appId: "1:844342741846:web:1a096ff331424904eae4f7"
+    apiKey: "-----------------------------",
+    authDomain: "-----------------------------",
+    databaseURL: "-------------------------------",
+    projectId: "-----------------------",
+    storageBucket: "----------------",
+    messagingSenderId: "----------",
+    appId: "--------------------------"
 };
